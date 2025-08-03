448c36d7868bd93834922d5e547f9e26134c29b5
thoxbox <sojobees@gmail.com>
2025-08-03 17:02:55 -0600

    Added a function in save called toJSONFormat. Converts a Layers object into a nested array structure.

--- src/filesaving.js
+++ src/filesaving.js
@@ -4,7 +4,14 @@ import Drawing from "./Drawing.js";
 
 /** @param {Layers} layers */
 function save(layers) {
-    
+    function toJSONFormat(layers) {
+        return layers.layers.map(layer => {
+            return layer.animation.map(frame => {
+                return frame.drawing.flat()
+            });
+        });
+    }
+    return toJSONFormat(layers);
 }
 
 /** @returns {Layers} */
