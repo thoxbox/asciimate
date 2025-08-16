287c1a63490f7b1b2493b5034131ea20364acb8d
thoxbox <sojobees@gmail.com>
2025-08-16 15:55:00 -0600

    Populated FileSaver.save and added a few optional arguments.

--- src/FileSaver.js
+++ src/FileSaver.js
@@ -1,9 +1,38 @@
 import { $ } from "./utils.js";
 
 class FileSaver {
-    static #downloader = $("#_downloader");
-    /** @param {Blob} blob @param {string} fileName */
-    static async save(blob, fileName) {
+    /** 
+     * @param {Blob} blob
+     * @param {string} mimeType
+     * @param {string | string[]} fileExtension
+     * @param {string} fileName
+     * @param {string} description */
+    static async save(blob, fileName, mimeType = null, fileExtension = null, description = null) {
+        if(window.showOpenFilePicker === undefined) {
+            const downloader = document.createElement("a");
+            const url = URL.createObjectURL(blob);
+            document.body.appendChild(downloader);
+            downloader.href = url;
+            downloader.download = fileName;
+            downloader.click();
+            downloader.remove();
+            URL.revokeObjectURL(url);
+            return;
+        }
+        const [fileHandle] = await window.showSaveFilePicker({
+            suggestedName: fileName,
+            types: [
+                {
+                    description : description ?? "",
+                    accept: {
+                        [mimeType ?? "application/unknown"]: [fileExtension ?? []].flat()
+                    },
+                },
+            ],
+        });
+        const writer = await fileHandle.createWritable();
+        await writer.write(blob);
+        await writer.close();
     }
     /** @returns {Blob} */
     static load() {
