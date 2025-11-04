b2905e1a0888f8b0c7c2f2e74eb8f02803fa0b26
thoxbox <sojobees@gmail.com>
2025-11-03 19:04:57 -0700

    Added constructor to Action

--- src/Action.js
+++ src/Action.js
@@ -1,3 +1,12 @@
-class Action {}
+class Action {
+    /** @type {Function} */
+    #undo = () => {};
+    /** @type {Function} */
+    #redo = () => {};
+    constructor({ undo = () => {}, redo = () => {} }) {
+        this.#undo = undo;
+        this.#redo = redo;
+    }
+}
 
 export default Action;
